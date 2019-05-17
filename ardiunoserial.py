4fd2ddadad26cedd9b259a4f7f54bab7a8d1706b
Andrew Sanchez <ans046@ucsd.edu>
2019-05-16 17:57:20 -0700

    init repository

---
+++ ardiunoserial.py
@@ -0,0 +1,11 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser = serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate=9600
+
+while True:
+
+  read_ser=ser.readline()
+  print(read_ser)
