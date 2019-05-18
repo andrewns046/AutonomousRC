94283858c2e43a49c5b071546502fcdb3c06bebc
Andrew Sanchez <ans046@ucsd.edu>
2019-05-17 22:27:08 -0700

    sends floats rather than integers

---
+++ serial_speedometer.py
@@ -0,0 +1,10 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser = serial.Serial("/dev/ttyACM0",9600)  # change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate=9600
+
+while True:
+  velocity = read(4) # expecting 4 bytes
+  print(velocity)
--- uno/speedometer/speedometer.ino
+++ uno/speedometer/speedometer.ino
@@ -1,6 +1,6 @@
 // Date Created:      May 17, 2019
 // Created By:        Andrew N. Sanchez
-// Project Titled:    Donkey Car Speedometer and Odometer
+// Project Titled:    Donkey Car Speedometer
 // Description:       Displays speed on 4 Digit 7 Segment but sends speed and distance through serial
 
 #include <FlexiTimer2.h>
@@ -8,7 +8,7 @@
 #define PI 3.1415926535897932384626433832795
 
 #define ENCODE_PIN_A 2
-#define RADIUS 3.2          // Set to units of distance
+#define RADIUS 3.2          // Set radius of 1 rev to units of distance
 #define PULSES_PER_REV 600  // LPD2806 Rotary Encoder gives 600 pulses per revolution
                             // Change this value according to your encoder
 #define LATCH_PIN 12        // Pin connected to ST_CP of 74HC595（Pin12）
@@ -23,8 +23,7 @@ byte num[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 
 volatile unsigned int counter = 0; // encoder counter
 volatile unsigned int revolutions = 0;  // encoder revolutions
 
-int velocity; 
-int distance;
+float velocity;
 
 void setup() {
   Serial.begin(9600);
@@ -32,7 +31,7 @@ void setup() {
   // set pins for encoder
   pinMode(ENCODE_PIN_A, INPUT); // set encoder pin
   digitalWrite(ENCODE_PIN_A, HIGH); // turn on pull up resistor
-  attachInterrupt(0, a_pin_trigger, RISING); // set interrupts for rising pulses sent from encoder
+  attachInterrupt(0, enc_trigger, RISING); // set interrupts for rising pulses sent from encoder
 
   // set pins for display
   pinMode(LATCH_PIN, OUTPUT);
@@ -49,27 +48,36 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(velocity);
-  Serial.print(distance);
+  send_speed();
   disp_speed();
 }
 
+void send_speed() {
+  float v = velocity;
+  byte * chunks = (byte *) &v;
+  Serial.write(chunks, 4);  // send float
+}
+
+//currently does not display precision values
 void disp_speed() {
+
+  int v = (int) velocity;  //get current speed
 
   // get digits to display
   byte bit[4];
-  bit[0] = velocity % 10;
-  bit[1] = velocity / 10 % 10;
-  bit[2] = velocity / 100 % 10;
-  bit[3] = velocity / 1000 % 10;
+  bit[0] = v % 10;
+  bit[1] = v / 10 % 10;
+  bit[2] = v / 100 % 10;
+  bit[3] = v / 1000 % 10;
 
   for (int i = 0; i < 4; i++) {
     // Select a single 7-segment display
     chooseCommon(i);
     // Send data to 74HC595
-    writeData(num[bit[3-i]]);
+    int write_val = num[bit[3-i]];
+    // write_val | 0x01                inject decimal
+    writeData(write_val);
     delay(5);
-    writeData(0xff);  // clear displayed value
   }
 }
 
@@ -94,7 +102,7 @@ void writeData(int value) {
 // INTERRUPT FUNCITONS
 
 // fires at rising encoder pulse
-void a_pin_trigger() {
+void enc_trigger() {
   counter++;
   if( counter == PULSES_PER_REV ) {
     revolutions++;
@@ -105,6 +113,5 @@ void a_pin_trigger() {
 // fires after 1 second has passed
 void calc_speed() {
   velocity = revolutions*(2*PI*RADIUS);
-  distance = (2*PI*RADIUS);
   revolutions = 0; // reset revolutions for next revolution count per second
 }
